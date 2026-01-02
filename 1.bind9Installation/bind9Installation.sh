a66d8e2c5f8fc1087f4a21e0f0c8eb9075917de4
arnold7800x3d <arnold.7800x3d@gmail.com>
2026-01-02 12:37:55 +0300

    added conf.options, .internal zones and reverse lookup

--- 1.bind9Installation/bind9Installation.sh
+++ 1.bind9Installation/bind9Installation.sh
@@ -33,14 +33,38 @@ setupBind9Server(){
     echo -e "Subtask 2.2 Verifying installation...\n"
     ssh $serverIPAddress "systemctl status bind9"
 
-    echo -e "Subtask 2.3 Creating the zone file...\n"
+    echo -e "Subtask 2.3 Editing the base configuration file /etc/bind/named.conf...\n"
+    ssh $serverIPAddress 'echo "include \"/etc/bind/named.conf.internal-zones\";" >> /etc/bind/named.conf'
+    ssh $serverIPAddress "cat /etc/bind/named.conf"
+
+    echo -e "Subtask 2.4 Configuring DNS forwarders and various DNS options...\n"
+    rsync -avz /home/netadmin/networkAutomationProjects/1.bind9Installation/templates/named.conf.options $serverIPAddress:/etc/bind/named.conf.options
+    ssh $serverIPAddress "cat /etc/bind/named.conf.options"
+
+    echo -e "Subtask 2.5 Adding zone for the domain...\n"
+    rsync -avz /home/netadmin/networkAutomationProjects/1.bind9Installation/templates/named.conf.internal-zones $serverIPAddress:/etc/bind/named.conf.internal-zones
+    
+    echo -e "Subtask 2.6 Creating the zone file...\n"
     rsync -avz /home/netadmin/networkAutomationProjects/1.bind9Installation/templates/db.asl.com $serverIPAddress:/etc/bind/db.asl.com
     ssh $serverIPAddress "ls /etc/bind/"
 
-    echo -e "Subtask 2.4 Updating the serial number of the zone file...\n"
+    echo -e "Subtask 2.7 Updating the serial number of the zone file...\n"
     local serial=`date +"%Y%m%d01"`
     ssh $serverIPAddress "sed -i 's/xxxxxxxxxx/$serial/' /etc/bind/db.asl.com"
     ssh $serverIPAddress "cat /etc/bind/db.asl.com"
+
+    echo -e "Subtask 2.8 Creating the reverse lookup file...\n"
+    rsync -avz /home/netadmin/networkAutomationProjects/1.bind9Installation/templates/db.100.168.192 $serverIPAddress:/etc/bind/db.100.168.192
+    ssh $serverIPAddress "ls /etc/bind"
+
+    echo -e "Subtask 2.9 Updating the serial number of the reverse lookup file...\n"
+    local serial=`date +"%Y%m%d01"`
+    ssh $serverIPAddress "sed -i 's/xxxxxxxxxx/$serial/' /etc/bind/db.100.168.192"
+    ssh $serverIPAddress "cat /etc/bind/db.100.168.192"
+
+    echo -e "Subtask 2.10 Restarting the DNS server...\n"
+    ssh $serverIPAddress "systemctl restart bind9"
+    ssh $serverIPAddress "systemctl status bind9"
 }
 
 setupBind9Server $serverIP
