fff57704d216823d9515e5db32ca37ee919e3439
arnold7800x3d <arnold.7800x3d@gmail.com>
2026-01-23 19:23:42 +0300

    Commented test file

--- 1.bind9Installation/test/bashTestFile.sh
+++ 1.bind9Installation/test/bashTestFile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# convert files into UNIX format
 dos2unix /home/netadmin/networkAutomationProjects/1.bind9Installation/bash/inventory/inventory.txt
 dos2unix /home/netadmin/networkAutomationProjects/1.bind9Installation/bash/bind9Installation.sh
 
+# make files executable
 chmod u+x /home/netadmin/networkAutomationProjects/1.bind9Installation/bash/inventory/inventory.txt
 chmod u+x /home/netadmin/networkAutomationProjects/1.bind9Installation/bash/bind9Installation.sh
 
+# run automation file
 /home/netadmin/networkAutomationProjects/1.bind9Installation/bash/bind9Installation.sh
